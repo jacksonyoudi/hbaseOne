dac72580d375c2b14666c7bd1d3e4477ad5850f9
jacksonliang <jacksonliang@zego.im>
2020-04-20 16:11:47 +0800

    init

---
+++ src/main/java/im/zego/App.java
@@ -0,0 +1,13 @@
+package im.zego;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
